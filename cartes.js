f9f03ee7bee5c48d20efb52baf2cf966a1e4778b
manonpierre <manon.pierre.2004@gmail.com>
2026-08-04 11:49:06 +0200

    Ajout des 20 cartes Mood Cards

---
+++ cartes.js
@@ -0,0 +1,22 @@
+const cartes = [
+    { image: "images/carte-01.jpg" },
+    { image: "images/carte-02.jpg" },
+    { image: "images/carte-03.jpg" },
+    { image: "images/carte-04.jpg" },
+    { image: "images/carte-05.jpg" },
+    { image: "images/carte-06.jpg" },
+    { image: "images/carte-07.jpg" },
+    { image: "images/carte-08.jpg" },
+    { image: "images/carte-09.jpg" },
+    { image: "images/carte-10.jpg" },
+    { image: "images/carte-11.jpg" },
+    { image: "images/carte-12.jpg" },
+    { image: "images/carte-13.jpg" },
+    { image: "images/carte-14.jpg" },
+    { image: "images/carte-15.jpg" },
+    { image: "images/carte-16.jpg" },
+    { image: "images/carte-17.jpg" },
+    { image: "images/carte-18.jpg" },
+    { image: "images/carte-19.jpg" },
+    { image: "images/carte-20.jpg" }
+];
