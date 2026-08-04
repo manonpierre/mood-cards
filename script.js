b16a003a35b35ac5b85b17217bee49a27537dddf
manonpierre <manon.pierre.2004@gmail.com>
2026-08-04 11:52:19 +0200

    Ajout du fonctionnement des cartes

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+let carteActuelle = 0;
+
+let choixClient = {
+    aime: [],
+    mitige: [],
+    pas: []
+};
+
+
 function startGame() {
-    alert("Bienvenue dans Mood Cards. Le jeu arrive bientôt !");
+
+    document.getElementById("accueil").style.display = "none";
+    document.getElementById("jeu").style.display = "block";
+
+    afficherCarte();
+
+}
+
+
+function afficherCarte() {
+
+    let carte = cartes[carteActuelle];
+
+    document.getElementById("imageCarte").src = carte.image;
+
+    document.getElementById("compteur").innerHTML =
+        "Carte " + (carteActuelle + 1) + " / " + cartes.length;
+
+}
+
+
+function choisir(reponse) {
+
+
+    choixClient[reponse].push(carteActuelle);
+
+
+    carteActuelle++;
+
+
+    if (carteActuelle < cartes.length) {
+
+        afficherCarte();
+
+    } else {
+
+        alert("Exploration terminée ! Merci pour vos réponses.");
+
+    }
+
 }
