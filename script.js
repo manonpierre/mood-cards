3af8aacc962421d0a975600c819b50dbf8c3790c
manonpierre <manon.pierre.2004@gmail.com>
2026-08-04 13:47:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 let carteActuelle = 0;
 
+
 let choixClient = {
+
     aime: [],
     mitige: [],
     pas: []
+
 };
+
 
 
 function startGame() {
 
+
     document.getElementById("accueil").style.display = "none";
-    document.getElementById("jeu").style.display = "block";
+
+    document.getElementById("jeu").style.display = "flex";
+
 
     afficherCarte();
 
 }
 
 
+
+
 function afficherCarte() {
+
 
     let carte = cartes[carteActuelle];
 
+
     document.getElementById("imageCarte").src = carte.image;
+
 
     document.getElementById("compteur").innerHTML =
         "Carte " + (carteActuelle + 1) + " / " + cartes.length;
 
+
+    resetBoutons();
+
+
 }
+
+
 
 
 function choisir(reponse) {
@@ -35,16 +53,66 @@ function choisir(reponse) {
     choixClient[reponse].push(carteActuelle);
 
 
-    carteActuelle++;
+    resetBoutons();
 
 
-    if (carteActuelle < cartes.length) {
+    document.getElementById(reponse)
+        .classList.add("selection");
+
+
+}
+
+
+
+
+function resetBoutons() {
+
+
+    document.getElementById("aime")
+        .classList.remove("selection");
+
+
+    document.getElementById("mitige")
+        .classList.remove("selection");
+
+
+    document.getElementById("pas")
+        .classList.remove("selection");
+
+
+}
+
+
+
+
+function carteSuivante() {
+
+
+    if (carteActuelle < cartes.length - 1) {
+
+
+        carteActuelle++;
+
 
         afficherCarte();
 
-    } else {
+    }
 
-        alert("Exploration terminée ! Merci pour vos réponses.");
+}
+
+
+
+
+function cartePrecedente() {
+
+
+    if (carteActuelle > 0) {
+
+
+        carteActuelle--;
+
+
+        afficherCarte();
 
     }
 
